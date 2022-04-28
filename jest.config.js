c05ff1e43c6f81fa0192121e885fb7fedbb7c957
SonOfMagic <1324318532@qq.com>
2022-04-28 12:50:08 +0800

    feat: commit jest-html-reporter

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,17 @@
 const config = {
   testEnvironment: "jsdom",
   verbose: true,
-  setupFilesAfterEnv: ['./jest.setup.js']
+  setupFilesAfterEnv: ['./jest.setup.js'],
+  testResultsProcessor: "./node_modules/jest-html-reporter",
+  reporters: [
+    "default",
+    ["jest-html-reporter", {
+      pageTitle: "Unit Tests",
+      outputPath: "./report/test-report.html",
+      includeFailureMsg: true      
+    }
+    ]
+  ],
+  coverageReporters: ['html']
 };
 module.exports = config;
